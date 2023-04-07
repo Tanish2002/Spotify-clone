7896c67489880bcf790d1033b0280855e15c0a72
Prakhar <prakharb56@gmail.com>
2023-04-07 09:40:23 +0530

    Adding auth

---
+++ spotify-clone/src/login/login.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = "0867525dd3864dceb604b5ad34c2ff02";
+const scopes = "user-top-read user-follow-read playlist-read-private user-library-read";
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+})
