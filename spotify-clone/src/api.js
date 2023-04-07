567202d2ea738a8e1b1f3fe1db45c6696948ac71
Prakhar <prakharb56@gmail.com>
2023-04-08 03:27:54 +0530

    changes

---
+++ spotify-clone/src/api.js
@@ -0,0 +1,2 @@
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
---
+++ spotify-clone/src/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+const loaduserProfile =()=>{
+    const defaultImage = document.querySelector("#default-image");
+    const profileButton = document.querySelector("#user-profile-btn");
+    const displayName = document.querySelector("#display-name")
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+})
+
