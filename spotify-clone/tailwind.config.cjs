c2e1dd094bd73ef3bf8e44da3e93fe1d10e2ea09
Prakhar <prakharb56@gmail.com>
2023-04-07 22:54:52 +0530

    styling for login page

--- spotify-clone/tailwind.config.cjs
+++ spotify-clone/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors:{
+         "green": "#1db954",
+         "dark": "#191414",
+         "white": "#fff",
+    },
     extend: {},
   },
   plugins: []
