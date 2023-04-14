bc28fa0df5144589c84929f96d60f0c5371b7981
Prakhar <prakharb56@gmail.com>
2023-04-14 07:45:48 +0530

    playlists added dynamically

--- spotify-clone/src/dashboard/dashboard.js
+++ spotify-clone/src/dashboard/dashboard.js
@@ -1,75 +1,86 @@
 import { fetchRequest } from "../api";
 import { ENDPOINT, logout } from "../common";
 
-
-const onProfileClick =(event)=>{
+const onProfileClick = (event) => {
   event.stopPropagation();
   const profileMenu = document.querySelector("#profile-menu");
   profileMenu.classList.toggle("hidden");
-  if(!profileMenu.classList.contains("hidden")){
-    profileMenu.querySelector("li#logout").addEventListener("click",logout);
+  if (!profileMenu.classList.contains("hidden")) {
+    profileMenu.querySelector("li#logout").addEventListener("click", logout);
   }
-}
-const loaduserProfile =async()=>{
-    const defaultImage = document.querySelector("#default-image");
-    const profileButton = document.querySelector("#user-profile-btn");
-    const displayNameElement = document.querySelector("#display-name");
+};
+const loaduserProfile = async () => {
+  const defaultImage = document.querySelector("#default-image");
+  const profileButton = document.querySelector("#user-profile-btn");
+  const displayNameElement = document.querySelector("#display-name");
 
-    const {display_name: displayName,images} = await fetchRequest(ENDPOINT.userInfo);
-   
+  const { display_name: displayName, images } = await fetchRequest(
+    ENDPOINT.userInfo
+  );
 
-    if(images?.length){
-      defaultImage.classList.add("hidden");
-    }else{
-        defaultImage.classList.remove("hidden");
-    }
+  if (images?.length) {
+    defaultImage.classList.add("hidden");
+  } else {
+    defaultImage.classList.remove("hidden");
+  }
 
-    profileButton.addEventListener("click",onProfileClick);
-    displayNameElement.textContent= displayName;
+  profileButton.addEventListener("click", onProfileClick);
+  displayNameElement.textContent = displayName;
+};
 
-}
+const onPlaylistItemClicked = (event) => {
+  console.log(event.target);
+};
 
-const onPlaylistItemClicked= (event)=>{
-console.log(event.target);
-}
-
-const loadPlaylist = async (endpoint ,elementId)=>{
-  const {playlists:{items}} = await fetchRequest(endpoint);
+const loadPlaylist = async (endpoint, elementId) => {
+  const {
+    playlists: { items },
+  } = await fetchRequest(endpoint);
   const playlistItemsSection = document.querySelector(`#${elementId}`);
- 
 
-
-  for(let {name ,description,images,id}of items){
+  for (let { name, description, images, id } of items) {
     const playlistItem = document.createElement("section");
-    playlistItem.className = "bg-black-secondary rounded p-4  hover:cursor-pointer hover:bg-light-black";
+    playlistItem.className ="bg-black-secondary rounded p-4  hover:cursor-pointer hover:bg-light-black";
     playlistItem.id = id;
-    playlistItem.setAttribute("data-type","playlist");
-    playlistItem.addEventListener("click",onPlaylistItemClicked);
-    const [{url:imageUrl}] = images;
-    playlistItem.innerHTML=
-    `<img src="${imageUrl}" alt="${name} class="rounded mb-2 object-contain shadow "/>
+    playlistItem.setAttribute("data-type", "playlist");
+    playlistItem.addEventListener("click", onPlaylistItemClicked);
+    const [{ url: imageUrl }] = images;
+    playlistItem.innerHTML = `<img src="${imageUrl}" alt="${name} class="rounded mb-2 object-contain shadow "/>
     <h2 class="text-base font-semibold mb-4 truncate">${name}</h2>
     <h3 class="text-sm text-secondary line-clamp-2">${description}</h3>`;
 
     playlistItemsSection.appendChild(playlistItem);
- 
   }
+};
 
+const loadPlaylists = () => {
+  loadPlaylist(ENDPOINT.featuredPlaylist, "featured-playlist-items");
+  loadPlaylist(ENDPOINT.toplists, "top-playlist-items");
+};
+
+
+const fillContentForDashboard = ()=>{
+  const pageContent = document.querySelector("#page-content")
+  const playlistMap = new Map([["featured", "featured-playlist-items"],["top playlists","top-playlist-items"]]);
+  let innerHTML = "";
+  for(let [type,id] of playlistMap){
+    innerHTML +=`<article class="p-4">
+    <h1 class="mb-4 text-2xl font-bold capitalize">${type}</h1>
+    <section
+      id="${id}"class="featured-songs grid grid-cols-auto-fill-cards gap-4"></section>
+  </article>
+    `  }
+    pageContent.innerHTML= innerHTML;
 }
 
-const loadPlaylists =()=>{
-  loadPlaylist(ENDPOINT.featuredPlaylist,"featured-playlist-items");
-  loadPlaylist(ENDPOINT.toplists,"top-playlist-items");
-}
-
-document.addEventListener("DOMContentLoaded",()=>{
-       loaduserProfile();
-       loadPlaylists();
-       document.addEventListener("click",()=>{
-        const profileMenu = document.querySelector("#profile-menu")
-        if(!profileMenu.classList.contains("hidden")){
-            profileMenu.classList.add("hidden")
-        }
-       })
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+  loaduserProfile();
+  loadPlaylists();
+  fillContentForDashboard();
+  document.addEventListener("click", () => {
+    const profileMenu = document.querySelector("#profile-menu");
+    if (!profileMenu.classList.contains("hidden")) {
+      profileMenu.classList.add("hidden");
+    }
+  });
+});
