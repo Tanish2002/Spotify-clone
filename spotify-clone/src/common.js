fcdb8f3c6031e74d4cc7b8b7d71ad393c74d0828
Prakhar <prakharb56@gmail.com>
2023-04-23 20:44:19 +0530

    adding next and prev func

--- spotify-clone/src/common.js
+++ spotify-clone/src/common.js
@@ -1,6 +1,7 @@
 export const ACCESS_TOKEN = "ACCESS_TOKEN";
 export const TOKEN_TYPE = "TOKEN_TYPE";
 export const EXPIRES_IN = "EXPIRES_IN";
+export const LOADED_TRACKS = "LOADED_TRACKS";
 const APP_URL = import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT = {
@@ -16,6 +17,9 @@ export const logout = ()=>{
     localStorage.removeItem(TOKEN_TYPE);
     window.location.href = `${APP_URL}/login/login.html`;
 }
+
+export const setItemInLocalStorage = (key , value)=>localStorage.setItem(key,JSON.stringify(value));
+export const getItemInLocalStorage = (key)=>JSON.parse(localStorage.getItem(key));
 
 export const SECTIONTYPE = {
     DASHBOARD: "DASHBOARD",
