8b67c4775183c22ff620d121bb2ca4da9525b7a6
Prakhar <prakharb56@gmail.com>
2023-05-01 21:23:26 +0530

    vite config updated

--- spotify-clone/vite.config.js
+++ spotify-clone/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-
+import { resolve } from 'path'
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
