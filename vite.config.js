464ce2a858ffe02e396b25f3d89e7befd5bc2064
Tanish2002 <tanishkhare@gmail.com>
2023-05-02 00:00:15 +0530

    update vite.config.js for multi-page-app

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    // vite config
-    define: {
-      __APP_ENV__: env.APP_ENV,
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        login: resolve(root, "login", "login.html"),
+        dashboard: resolve(root, "dashboard", "dashboard.html"),
+      },
     },
-    root: "src",
-  }
-})
+  },
+});
